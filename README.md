6fe31763a1a84a683f8bdfe2b0f19e62e5d7b578
cristiansep <cristiansepulvedamendez@gmail.com>
2019-10-21 21:43:58 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Aplicacion del clima
+
+Ejecutar ```npm install``` para las librerias
+
+
+## Ejemplo para ver clima
+
+```
+node app -d Osorno
+```
---
+++ clima/clima.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+
+
+const getClima=async(lat,lng)=>{
+
+   const resp=await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=bd4001c4e3b0722ddab376a0c4ad2995&units=metric`);
+    
+    return resp.data.main.temp;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports={
+    getClima
+}
